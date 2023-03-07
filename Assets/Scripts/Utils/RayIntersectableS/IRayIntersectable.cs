6b254dfd75474fa46b115b501beaa892f3e97ca3
MarkusSecundus <kubahronik.g@gmail.com>
2023-03-08 00:08:26 +0100

    started work on generic IRayIntersectable visualization

--- Assets/Scripts/Utils/RayIntersectableS/IRayIntersectable.cs
+++ Assets/Scripts/Utils/RayIntersectableS/IRayIntersectable.cs
@@ -1,10 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
 
-
-public interface IRayIntersectable
+public abstract class IRayIntersectable : MonoBehaviour
 {
-    public Vector3? GetIntersection(Ray r);
+    public abstract Vector3? GetIntersection(Ray r);
+}
+
+public static class RayIntersectableExtensions
+{
+    public static void Visualize(this IRayIntersectable self, Camera camera, DrawHelpers.LineDrawer<Vector3> drawLine)
+    {
+        var plane = new Plane(new Vector3(0, 0, 1), Vector3.zero);
+        DrawHelpers.DrawPlaneSegment(plane, Vector3.zero, LineDrawer);
+
+        void LineDrawer(Vector3 a, Vector3 b)
+        {
+            var (v, w) = (transform(a), transform(b));
+            if (v != null && w != null)
+                drawLine(v.Value, w.Value);
+
+            Vector3? transform(Vector3 v)
+            {
+                //var ray = new Ray(v.Value, plane.normal);
+                var ray = camera.ScreenPointToRay(v);
+                return self.GetIntersection(ray);
+            }
+        }
+    }
 }
--- Assets/Scripts/Utils/RayIntersectableS/RayIntersectableSphere.cs
+++ Assets/Scripts/Utils/RayIntersectableS/RayIntersectableSphere.cs
@@ -3,16 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public struct RayIntersectableSphere : IRayIntersectable
+public class RayIntersectableSphere : IRayIntersectable
 {
     public Transform Center;
     public float Radius;
+    public bool DrawGizmo = false;
 
-    public Vector3? GetIntersection(Ray r)
+    public override Vector3? GetIntersection(Ray r)
     {
         var ret = r.IntersectSphere(new Sphere(Center.position, Radius));
         if (ret.First != null) return ret.First.Value;
         if (ret.Second != null)return ret.Second.Value;
         return null;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (!DrawGizmo) return;
+
+        Gizmos.color = Color.red;
+        DrawHelpers.DrawWireSphere(Center.position, Radius, Gizmos.DrawLine);
     }
 }
