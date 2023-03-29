d0729f7c683a6ff2caff60a1e470ddec2796ee04
MarkusSecundus <kubahronik.g@gmail.com>
2023-03-29 23:38:05 +0200

    first draft of sword damage computation

--- Assets/Scripts/Sword/Damage/SwordWeapon.cs
+++ Assets/Scripts/Sword/Damage/SwordWeapon.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SwordWeapon : MonoBehaviour, IWeapon
 {
     [SerializeField] SwordDescriptor descriptor;
+
+    [SerializeField] float EdgeSharpness = 1f, SideDullingExponent = 0.3f;
+
     private void Awake()
     {
         descriptor = descriptor.IfNil(GetComponentInParent<SwordDescriptor>());
@@ -12,9 +16,16 @@ public class SwordWeapon : MonoBehaviour, IWeapon
 
     public Vector3 CalculateDamage_impl(Collision collision)
     {
+        return descriptor.EdgeDirections.Select(computeDamage).Maximum(v=>v.magnitude);
 
-
-        return default;
+        Vector3 computeDamage(Vector3 edgeDirection)
+        {
+            var directionRatio = edgeDirection.normalized.Dot(collision.impulse.normalized);
+            directionRatio = Mathf.Abs(directionRatio);
+            directionRatio = Mathf.Pow(directionRatio, SideDullingExponent);
+            var sharpnessRatio = directionRatio * EdgeSharpness;
+            return collision.impulse * sharpnessRatio;
+        }
     }
 
 }
