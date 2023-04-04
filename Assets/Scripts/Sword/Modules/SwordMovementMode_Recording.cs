709cb601591f3ecc4267eb9500f4376054e11cc7
MarkusSecundus <kubahronik.g@gmail.com>
2023-04-04 15:27:02 +0200

    started work on sword commands recording

---
+++ Assets/Scripts/Sword/Modules/SwordMovementMode_Recording.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using static SwordMovement;
+
+public class SwordMovementMode_Recording : SwordMovement.Submodule, ISwordMovement
+{
+    public SwordDescriptor Sword => Script.Sword;
+    public ISwordInput Input => Script.Input;
+    
+    public UnityEvent<ISwordMovement.MovementCommand> MovementReporter = new UnityEvent<ISwordMovement.MovementCommand>();
+    public ScriptSubmodulesContainer<KeyCode, Submodule, ISwordMovement> Modes;
+    IScriptSubmodule<ISwordMovement> submoduleManager;
+    protected override void OnStart()
+    {
+        base.OnStart();
+        submoduleManager = new ScriptSubmoduleListManager<KeyCode, Submodule, ISwordMovement>() { ActivityPredicate = Input.GetKey, Modes = Modes }.Init(this);
+    }
+    public override void OnUpdate(float delta) => submoduleManager.OnUpdate(delta);
+    public override void OnDrawGizmos() => submoduleManager?.OnDrawGizmos();
+    public override void OnFixedUpdate(float delta) => submoduleManager.OnFixedUpdate(Time.fixedDeltaTime);
+
+    public void MoveSword(ISwordMovement.MovementCommand m)
+    {
+        MovementReporter.Invoke(m);
+        Script.MoveSword(m);
+    }
+}
