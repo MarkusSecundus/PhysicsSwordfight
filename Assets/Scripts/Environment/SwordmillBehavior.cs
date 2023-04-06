03ebd44901167546541004f392b5e02c6f88e32f
MarkusSecundus <kubahronik.g@gmail.com>
2023-04-06 12:30:57 +0200

    fixed Swordmill assembly glitching on creation

--- Assets/Scripts/Environment/SwordmillBehavior.cs
+++ Assets/Scripts/Environment/SwordmillBehavior.cs
@@ -8,34 +8,14 @@ public class SwordmillBehavior : MonoBehaviour
     public ConfigurableJoint Rotor;
     public ConfigurableJoint SwordPrototype;
     public int SwordsCount = 1;
-    public Vector3 positionOffset = Vector3.zero;   
 
     public float SwordsAngle = 0f;
 
     private IEnumerable<ConfigurableJoint> swords;
-    private Vector3 originalConnectedAnchor;
     void Start()
     {
         swords = MakeSwords(SwordPrototype, SwordsCount);
-        originalConnectedAnchor = Rotor.connectedAnchor;
-        Rotor.autoConfigureConnectedAnchor = false;
     }
-
-
-
-    void Update()
-    {
-        //foreach (var s in swords) s.targetRotation = Quaternion.Euler(s.targetRotation.eulerAngles.With(x: SwordsAngle));
-
-        if(Rotor!= null)
-            Rotor.connectedAnchor = originalConnectedAnchor + positionOffset;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log($"Collision: {gameObject.name} - {collision.gameObject.name}");
-    }
-
 
 
     IEnumerable<ConfigurableJoint> MakeSwords(ConfigurableJoint proto, int count)
@@ -49,10 +29,15 @@ public class SwordmillBehavior : MonoBehaviour
         foreach (var v in GeometryUtils.PointsOnCircle(count, proto.connectedAnchor, Vector3.up, includeBegin:true))
         {
             var s = proto.gameObject.InstantiateWithTransform().GetComponent<ConfigurableJoint>();
+            var rotationHelper = s.MakeRotationHelper();
             ret.Add(s);
+
             s.connectedAnchor = v;
-            s.targetRotation = s.targetRotation * Quaternion.AngleAxis(Op.post_assign(ref angleAccumulator, angleAccumulator + angleToAdd), Vector3.up);
-            //s.transform.position = s.connectedBody.transform.LocalToGlobal(s.connectedAnchor);
+            s.transform.position = s.connectedBody.transform.LocalToGlobal(s.connectedAnchor);
+            var angle = s.targetRotation * Quaternion.AngleAxis(Op.post_assign(ref angleAccumulator, angleAccumulator + angleToAdd), Vector3.up) ;
+            rotationHelper.SetTargetRotation(angle);
+            s.transform.rotation = angle;
+            
             s.gameObject.SetActive(true);
         }
 
