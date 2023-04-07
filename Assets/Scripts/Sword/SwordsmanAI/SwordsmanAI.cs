5933edf799e480f529be3033a5e7b0304de39139
MarkusSecundus <kubahronik.g@gmail.com>
2023-04-07 20:23:10 +0200

    AI swordsman's navmesh agent now doesn't get broken on spawn

--- Assets/Scripts/Sword/SwordsmanAI/SwordsmanAI.cs
+++ Assets/Scripts/Sword/SwordsmanAI/SwordsmanAI.cs
@@ -27,6 +27,7 @@ public class SwordsmanAI : MonoBehaviour
     {
         agent = GetComponentInChildren<NavMeshAgent>();
         agent.DisableAllUpdates();
+        agent.Warp(agent.transform.position);
     }
 
 
